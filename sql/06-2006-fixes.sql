94aea35ecc81dcaa2bb67e974eb433df18b5e460
Tim Cooper <tim.cooper@layeh.com>
2016-05-29 14:51:53 -0300

    add 2006 SQL fixes

---
+++ sql/06-2006-fixes.sql
@@ -0,0 +1,56 @@
+-- https://cwsf.youthscience.ca/sites/default/files/documents/cwsf/2006%20CWSF%20Awards.pdf
+
+BEGIN TRANSACTION;
+
+-- 500
+UPDATE finalists SET city = "Leader", province = "SK" WHERE project = 500;
+
+-- 511
+UPDATE projects SET region = "Chignecto East" WHERE id = 511;
+INSERT INTO finalists
+(project, member, name, city, province, gender, biography)
+VALUES (
+  511,
+  1,
+  "Alissa Ali",
+  NULL, -- TODO
+  "NS",
+  NULL,
+  NULL
+);
+
+-- 557
+UPDATE projects SET region = "London District" WHERE id = 557;
+INSERT INTO finalists
+(project, member, name, city, province, gender, biography)
+VALUES (
+  557,
+  1,
+  "Ratna Varma",
+  NULL, -- TODO
+  "ON",
+  NULL,
+  NULL
+);
+
+-- 596
+UPDATE projects SET region = "Edmonton" WHERE id = 596;
+INSERT INTO finalists
+(project, member, name, city, province, gender, biography)
+VALUES (
+  596,
+  1,
+  "Gary Kurek",
+  "Fort Kent",
+  "AB",
+  NULL,
+  NULL
+);
+
+-- 636
+UPDATE finalists SET city = "Taipei" WHERE project = 636;
+
+-- 740
+UPDATE finalists SET city = "Hamilton", province = "ON" WHERE project = 740;
+
+COMMIT;
