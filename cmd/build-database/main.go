42098594c5a1c59f906340c0131fe5c5593d12b7
Tim Cooper <tim.cooper@layeh.com>
2016-05-29 09:35:51 -0300

    add build-database

---
+++ cmd/build-database/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"database/sql"
+	"os"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	dir := flag.String("dir", "sql", "SQL file directory")
+	filename := flag.String("filename", "database.sqlite3", "SQLite3 database file")
+	flag.Parse()
+
+	db, err := sql.Open("sqlite3", *filename)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	files, err := ioutil.ReadDir(*dir)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+		os.Exit(1)
+	}
+
+	for _, fileinfo := range files {
+		name := filepath.Join(*dir, fileinfo.Name())
+		contents, err := ioutil.ReadFile(name)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err)
+			os.Exit(1)
+		}
+		if _, err := db.Exec(string(contents)); err != nil {
+			fmt.Fprintln(os.Stdout, err)
+			os.Exit(1)
+		}
+	}
+}
