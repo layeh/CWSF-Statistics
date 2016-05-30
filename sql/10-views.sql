54091868d816b8c37619d5422ac2b670bbcb49fd
Tim Cooper <tim.cooper@layeh.com>
2016-05-29 21:56:44 -0300

    add projects_missing_finalist view

--- sql/10-views.sql
+++ sql/10-views.sql
@@ -1,15 +1,23 @@
 BEGIN TRANSACTION;
 
+-- Projects without at least one finalist
+CREATE VIEW projects_missing_finalist AS
+  SELECT projects.*
+  FROM projects
+    LEFT OUTER JOIN finalists
+      ON projects.id = finalists.project
+  WHERE finalists.project IS NULL;
+
 -- Total prizes awarded
-CREATE VIEW total_prizes (year, value) AS
+CREATE VIEW total_prizes AS
   SELECT
     fairs.year AS year,
     SUM(awards.value) AS value
   FROM fairs
-  LEFT JOIN projects
-  ON fairs.year = projects.year
-  LEFT JOIN awards
-  ON projects.id = awards.project
+    LEFT JOIN projects
+      ON fairs.year = projects.year
+    LEFT JOIN awards
+      ON projects.id = awards.project
   GROUP BY projects.year;
 
 COMMIT;
