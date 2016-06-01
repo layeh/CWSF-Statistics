fa013c32683e37075069950aadb7c9321067eee9
Tim Cooper <tim.cooper@layeh.com>
2016-05-31 21:18:48 -0300

    remove leading/trailing spaces from project titles

--- sql/09-cleanups.sql
+++ sql/09-cleanups.sql
@@ -1,11 +1,14 @@
 BEGIN TRANSACTION;
 
--- Remove excess spaces in finalist names
+-- Remove excess spaces in finalists
 UPDATE finalists SET name = REPLACE(name, "     ", " ");
 UPDATE finalists SET name = REPLACE(name, "    ", " ");
 UPDATE finalists SET name = REPLACE(name, "   ", " ");
 UPDATE finalists SET name = REPLACE(name, "  ", " ");
 UPDATE finalists SET name = TRIM(name);
 UPDATE finalists SET city = TRIM(city);
+
+-- Remove leading/trailing spaces in project titles
+UPDATE projects SET title = TRIM(title);
 
 COMMIT;
