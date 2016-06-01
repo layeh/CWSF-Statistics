0e1df9eb318c84d0c5c0a41fb5b8c024e63b751a
Tim Cooper <tim.cooper@layeh.com>
2016-05-31 21:16:24 -0300

    trim finalist city

--- sql/09-cleanups.sql
+++ sql/09-cleanups.sql
@@ -6,5 +6,6 @@ UPDATE finalists SET name = REPLACE(name, "    ", " ");
 UPDATE finalists SET name = REPLACE(name, "   ", " ");
 UPDATE finalists SET name = REPLACE(name, "  ", " ");
 UPDATE finalists SET name = TRIM(name);
+UPDATE finalists SET city = TRIM(city);
 
 COMMIT;
